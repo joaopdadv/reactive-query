2ddf31048115de4ad110e1bbe1fa0b98480a45c6
joaopdadv <joaopdadv@gmail.com>
2025-08-28 16:14:33 -0300

    cria monorepo

--- packages/lib/tsup.config.ts
+++ packages/lib/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "tsup";
+
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
@@ -7,5 +8,5 @@ export default defineConfig({
   treeshake: true,
   clean: true,
   minify: false,
-  external: []
+  external: ["react", "react-dom"],
 });
